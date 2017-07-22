2e82e9419e8f63c0f1ea8096b318f007d37dd96e
Liam W <l.s.w5559@gmail.com>
2017-07-22 20:16:43 +0100

    Initial version

---
+++ _output/extension_hint.php
@@ -0,0 +1,9 @@
+<?php
+
+// ################## THIS IS A GENERATED FILE ##################
+// DO NOT EDIT DIRECTLY. EDIT THE CLASS EXTENSIONS IN THE CONTROL PANEL.
+
+namespace SV\RichUsernameSupport\XF\Entity
+{
+	class XFCP_Thread extends \XF\Entity\Thread {}
+}
